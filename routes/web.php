1e32a457310685ee529438d3d6db76557787dc48
Lannchong <meilannoaditya08@gmail.com>
2025-11-02 13:33:14 +0700

    Tugas Laravel 02: Implement landing page & AdminLTE Dashboard

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\LandingController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+
+Route::get('/', [LandingController::class, 'index'])->name('index');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+});
